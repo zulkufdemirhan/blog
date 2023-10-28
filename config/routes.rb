31fc64bffab79a2f7e3fcf56b7b7eac1935df51f
Zülküf Demirhan <95721351+zulkufdemirhan@users.noreply.github.com>
2023-10-28 22:42:05 +0300

    rails blog created

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  resources :blog_posts, except: [:index]
+
+  root "blog_posts#index"
+end
