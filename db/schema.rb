b522a977ed55594eb7f1355ec12cf1d5cac31f07
Zülküf Demirhan <95721351+zulkufdemirhan@users.noreply.github.com>
2023-11-01 23:12:26 +0300

    added published_at for draft, published and scheduled blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_30_195223) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_01_191819) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
