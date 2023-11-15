e883a96c0bdd6475c6f0891673d412571ead62d9
Zülküf Demirhan <95721351+zulkufdemirhan@users.noreply.github.com>
2023-11-16 00:23:12 +0200

    tailwind customized, login page created and other minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 user = User.where(email: "test@gmail.com").first_or_initialize
 user.update!(
-  password: "password",
-  password_confirmation: "password"
+  password: "qweqwe",
+  password_confirmation: "qweqwe"
 )
 
 # 100.times do |i|
